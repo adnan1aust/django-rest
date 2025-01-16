75b3cc164c396cf3cbadc338e93263d32e6eb8fd
Ahmed Shahriar Adnan <adnan1aust@gmail.com>
2025-01-16 14:14:05 +0600

    FEAT: Created a basic GET API

---
+++ backend/api/views.py
@@ -0,0 +1,17 @@
+import json
+from django.http import JsonResponse
+
+def api_home(request, *args, **kwargs):
+    print(request.GET) #url query params
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    #data['headers'] = request.headers
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,6 +1,7 @@
 import requests
 
-endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8080/api/"
 
-response = requests.get(endpoint, json={"query": "Hello World"}) #Emulate HTTP get request
+response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello World"}) #Emulate HTTP get request
 print(response.json()) #Print the response
+#print(response.status_code)
