23dc2b7ff36e985d8f5c9020e5dda1e9c76c354f
Ahmed Shahriar Adnan <adnan1aust@gmail.com>
2025-01-16 15:29:05 +0600

    FEAT: Replace django GET with DRF GET

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,26 @@
 import json
-from django.forms.models import model_to_dict
-from django.http import JsonResponse
-
 from products.models import Product
 
-def api_home(request, *args, **kwargs):
+""" from django.forms.models import model_to_dict
+from django.http import JsonResponse """
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from products.serializers import ProductSerializer
+
+
+""" def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
         data = model_to_dict(model_data)
         #json_string = josn.dumps(data)
-    return JsonResponse(data)
+    return JsonResponse(data) """
+
+@api_view(['GET'])
+def api_home(request, *args, **kwargs):
+    instance = Product.objects.all().order_by("?").first()
+    data = {}
+    if instance:
+        data = ProductSerializer(instance).data
+    return Response(data)
